486e30d269ab227a228b25b92cb914397e213b64
novokshonov.nikita <Nikita.Novokhnov@gmail.com>
2024-07-28 17:59:52 +0500

    stat-service 1

---
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/service/StatsService.java
@@ -0,0 +1,13 @@
+package ru.practicum.stats.server.service;
+
+import ru.practicum.stats.service.dto.EventRequest;
+import ru.practicum.stats.service.dto.StatisticResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatsService {
+    List<StatisticResponse> getStatistics(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
+
+    void addEvent(EventRequest eventRequest);
+}
---
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/service/StatsServiceImpl.java
@@ -0,0 +1,50 @@
+package ru.practicum.stats.server.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.stats.server.mapper.StatsMapper;
+import ru.practicum.stats.server.repository.StatsRepository;
+import ru.practicum.stats.service.dto.EventRequest;
+import ru.practicum.stats.service.dto.StatisticResponse;
+
+import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StatsServiceImpl implements StatsService {
+
+    private final StatsRepository repository;
+    private final StatsMapper mapper;
+
+    @Override
+    public List<StatisticResponse> getStatistics(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        if (start.isAfter(end)) {
+            throw new ConstraintViolationException("Дата начала позже даты старта", null);
+        }
+
+        if (uris == null || uris.isEmpty()) {
+            if (unique) {
+                return repository.getAllStatisticsUniqueIp(start, end).stream().map(mapper::toStatisticResponse).collect(Collectors.toList());
+            } else {
+                return repository.getAllStatisticsAnyIp(start, end).stream().map(mapper::toStatisticResponse).collect(Collectors.toList());
+            }
+        } else {
+            if (unique) {
+                return repository.getStatisticsUniqueIp(start, end, uris).stream().map(mapper::toStatisticResponse).collect(Collectors.toList());
+            } else {
+                return repository.getStatisticsAnyIp(start, end, uris).stream().map(mapper::toStatisticResponse).collect(Collectors.toList());
+            }
+        }
+    }
+
+    @Override
+    @Transactional
+    public void addEvent(EventRequest eventRequest) {
+        repository.save(mapper.toEvent(eventRequest));
+    }
+}
