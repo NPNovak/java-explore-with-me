17d0d0a8d301166aab0b8c0f712a06ec0c6a719a
novokshonov.nikita <Nikita.Novokhnov@gmail.com>
2024-08-19 01:02:36 +0500

    feature 3

--- main-service/src/main/java/ru/practicum/main/service/service/CategoryService.java
+++ main-service/src/main/java/ru/practicum/main/service/service/CategoryService.java
@@ -1,65 +1,25 @@
 package ru.practicum.main.service.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.main.service.dto.category.CategoryResponse;
 import ru.practicum.main.service.dto.category.NewCategoryRequest;
-import ru.practicum.main.service.exception.model.NotExistException;
-import ru.practicum.main.service.mapper.CategoryMapper;
 import ru.practicum.main.service.model.Category;
-import ru.practicum.main.service.repository.CategoryRepository;
 
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+public interface CategoryService {
+    List<CategoryResponse> getCategories(int from, int size);
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class CategoryService {
-
-    private final CategoryRepository repository;
-    private final CategoryMapper mapper;
-
-    public List<CategoryResponse> getCategories(int from, int size) {
-        return repository.findAll(PageRequest.of(from / size, size))
-                .stream()
-                .map(mapper::toCategoryDto)
-                .collect(toList());
-    }
-
-    public CategoryResponse getCategory(long catId) {
-        return mapper.toCategoryDto(findCategory(catId));
-    }
+    CategoryResponse getCategory(long catId);
 
     @Transactional
-    public CategoryResponse addCategory(NewCategoryRequest newCategoryRequest) {
-        return mapper.toCategoryDto(repository.save(mapper.toCategory(newCategoryRequest)));
-    }
+    CategoryResponse addCategory(NewCategoryRequest newCategoryRequest);
 
     @Transactional
-    public CategoryResponse updateCategory(long catId, NewCategoryRequest newCategoryRequest) {
-        Category category = findCategory(catId);
-
-        if (category.getName().equals(newCategoryRequest.getName())) {
-            return mapper.toCategoryDto(category);
-        }
-
-        category.setName(newCategoryRequest.getName());
-
-        return mapper.toCategoryDto(repository.save(category));
-    }
+    CategoryResponse updateCategory(long catId, NewCategoryRequest newCategoryRequest);
 
     @Transactional
-    public void deleteCategory(long catId) {
-        findCategory(catId);
-        repository.deleteById(catId);
-    }
+    void deleteCategory(long catId);
 
-    private Category findCategory(long catId) {
-        return repository.findById(catId)
-                .orElseThrow(() -> new NotExistException("Category с id = " + catId + " не существует"));
-    }
+    Category findCategory(long catId);
 }
---
+++ main-service/src/main/java/ru/practicum/main/service/service/UserServiceImpl.java
@@ -0,0 +1,166 @@
+package ru.practicum.main.service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.main.service.dto.event.EventShortResponse;
+import ru.practicum.main.service.dto.user.NewUserRequest;
+import ru.practicum.main.service.dto.user.UserResponse;
+import ru.practicum.main.service.enums.StateEvent;
+import ru.practicum.main.service.enums.StateRequest;
+import ru.practicum.main.service.exception.model.ConflictException;
+import ru.practicum.main.service.exception.model.NotExistException;
+import ru.practicum.main.service.mapper.CategoryMapper;
+import ru.practicum.main.service.mapper.EventMapper;
+import ru.practicum.main.service.mapper.UserMapper;
+import ru.practicum.main.service.model.Request;
+import ru.practicum.main.service.model.User;
+import ru.practicum.main.service.repository.EventRepository;
+import ru.practicum.main.service.repository.RequestRepository;
+import ru.practicum.main.service.repository.UserRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+    private final UserMapper mapper;
+    private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
+    private final RequestRepository requestRepository;
+    private final CategoryMapper categoryMapper;
+
+    @Override
+    public List<UserResponse> getUsers(List<Long> ids, int from, int size) {
+
+        Pageable pageable = PageRequest.of(from / size, size);
+        Page<User> users;
+
+        if (ids == null || ids.isEmpty()) {
+            users = repository.findAll(pageable);
+        } else {
+            users = repository.findAllByIdIn(ids, pageable);
+        }
+
+        return users
+                .stream()
+                .map(mapper::toUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<EventShortResponse> getFriendRequests(long userId, long friendId) {
+        if (userId == friendId) {
+            throw new ConflictException("Указаны одинаковые id, userId = " + userId + ", friendId = " + friendId);
+        }
+
+        User user = checkUser(userId);
+
+        if (!user.getFriends().contains(friendId)) {
+            throw new NotExistException("User с id = " + userId + " не имеет в друзьях friendId = " + friendId);
+        }
+
+        Set<Long> eventIds = requestRepository.findByRequesterIdAndStatus(friendId, StateRequest.CONFIRMED)
+                .stream()
+                .map(Request::getEventId)
+                .collect(Collectors.toSet());
+
+        return eventRepository
+                .findByIdInAndEventDateAfterAndStateOrderByEventDateAsc(
+                        eventIds,
+                        LocalDateTime.now(),
+                        StateEvent.PUBLISHED)
+                .stream()
+                .map(event -> eventMapper.toEventShortResponse(event,
+                        categoryMapper.toCategoryDto(event.getCategory()),
+                        mapper.toUserShortResponse(event.getInitiator())))
+                .collect(toList());
+    }
+
+    @Override
+    public List<EventShortResponse> getFriendsRequests(long userId) {
+        User user = checkUser(userId);
+
+        if (user.getFriends().isEmpty()) {
+            return List.of();
+        }
+
+        Set<Long> eventIds = requestRepository.findByRequesterIdInAndStatus(user.getFriends(), StateRequest.CONFIRMED)
+                .stream()
+                .map(Request::getEventId)
+                .collect(Collectors.toSet());
+
+        return eventRepository
+                .findByIdInAndEventDateAfterAndStateOrderByEventDateAsc(
+                        eventIds,
+                        LocalDateTime.now(),
+                        StateEvent.PUBLISHED)
+                .stream()
+                .map(event -> eventMapper.toEventShortResponse(event,
+                        categoryMapper.toCategoryDto(event.getCategory()),
+                        mapper.toUserShortResponse(event.getInitiator())))
+                .collect(toList());
+    }
+
+    @Override
+    @Transactional
+    public UserResponse addUser(NewUserRequest newUserRequest) {
+        return mapper.toUserResponse(repository.save(mapper.toUser(newUserRequest)));
+    }
+
+    @Override
+    @Transactional
+    public UserResponse addFriend(long userId, long friendId) {
+        if (userId == friendId) {
+            throw new ConflictException("Указаны одинаковые id, userId = " + userId + ", friendId = " + friendId);
+        }
+
+        User user = checkUser(userId);
+
+        if (user.getFriends().contains(friendId)) {
+            throw new ConflictException("User с id = " + userId + " уже является другом с friendId = " + friendId);
+        }
+
+        user.getFriends().add(checkUser(friendId).getId());
+
+        return mapper.toUserResponse(repository.save(user));
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long userId) {
+        repository.findById(userId).orElseThrow(() -> new NotExistException("User с id = " + userId + " не существует"));
+        repository.deleteById(userId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteFriend(long userId, long friendId) {
+        if (userId == friendId) {
+            throw new ConflictException("Указаны одинаковые id, userId = " + userId + ", friendId = " + friendId);
+        }
+
+        User user = checkUser(userId);
+
+        if (!user.getFriends().remove(friendId)) {
+            throw new NotExistException("User с id = " + userId + " не является другом с friendId = " + friendId);
+        }
+
+        repository.save(user);
+    }
+
+    public User checkUser(long userId) {
+        return repository.findById(userId)
+                .orElseThrow(() -> new NotExistException("User с id = " + userId + " не существует"));
+    }
+}
